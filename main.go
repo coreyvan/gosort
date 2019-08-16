8c0b3543719dee73dc6be3aa9e2791139e39d9b6
Corey Van Woert <coreyvan@protonmail.com>
2019-08-16 10:11:03 +0100

    Clean up code and add comments

--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
 	"time"
 )
 
 const (
-	size int = 10
+	size int = 10000
 )
 
+// timeTaken measures the time taken for a function to return
 func timeTaken(t time.Time, name string) {
 	elapsed := time.Since(t)
 	log.Printf("TIME: %s\t\t%s\n", name, elapsed)
 }
+
+// swap returns the inputs in swapped order
 func swap(i int, j int) (int, int) {
 	return j, i
 }
 
+// generateRandomSlice generates a slice of ints of the size specified from 0-199
 func generateRandomSlice(size int) []int {
 	slice := make([]int, size, size)
 	rand.Seed(time.Now().UnixNano())
@@ -28,22 +31,23 @@ func generateRandomSlice(size int) []int {
 	return slice
 }
 
+//bubbleSort sorts the input array with a bubble sort algorithm
 func bubbleSort(inArr []int) {
 	defer timeTaken(time.Now(), "bubbleSort")
 	arr := inArr
 	n := 1
 	for n < len(arr) {
 		for i := 0; i < len(arr)-n; i++ {
-			// compare arr[0] vs arr[1]
+			// compare arr[0] vs arr[1] and swap
 			if arr[i] > arr[i+1] {
 				arr[i], arr[i+1] = swap(arr[i], arr[i+1])
 			}
 		}
 		n++
 	}
-	// fmt.Printf("Sorted: %v\n", arr)
 }
 
+// selectSort sorts the input array with a select sort algorithm
 func selectSort(arr []int) {
 	defer timeTaken(time.Now(), "selectSort")
 	for i := 0; i < len(arr); i++ {
@@ -55,9 +59,9 @@ func selectSort(arr []int) {
 			}
 		}
 	}
-	// fmt.Printf("Sorted: %v\n", arr)
 }
 
+// insertionSort sorts the input array with an insertion sort algorithm
 func insertionSort(arr []int) {
 	defer timeTaken(time.Now(), "insertionSort")
 	for i := 0; i < len(arr); i++ {
@@ -70,18 +74,16 @@ func insertionSort(arr []int) {
 		}
 		arr[pos] = cursor
 	}
-	// fmt.Printf("Sorted: %v\n", arr)
 }
 
-func mergeSortWrapper(arr []int) []int {
-	fmt.Printf("Initial array: %v\n", arr)
+// mergeSortWrapper wraps the recursive function mergeSort
+func mergeSortWrapper(arr []int) {
 	defer timeTaken(time.Now(), "mergeSort")
-	merged := mergeSort(arr)
-	fmt.Printf("Merge sorted: %v\n", merged)
-	return merged
+	_ = mergeSort(arr)
 }
-func mergeSort(arr []int) []int {
 
+// mergeSort recursively sorts the input array using a merge sort algorithm
+func mergeSort(arr []int) []int {
 	if len(arr) <= 1 {
 		return arr
 	}
@@ -89,10 +91,10 @@ func mergeSort(arr []int) []int {
 	mid := len(arr) / 2
 	left, right := mergeSort(arr[:mid]), mergeSort(arr[mid:])
 	merged := merge(left, right)
-	// fmt.Printf("Input: %v\nMid: %v\nLeft: %v\nRight: %v\nMerged: %v\n\n", arr, mid, left, right, merged)
 	return merged
 }
 
+// merge returns a merged sorted array using the two input arrays
 func merge(l []int, r []int) []int {
 	merged := make([]int, len(l)+len(r))
 
@@ -120,17 +122,17 @@ func merge(l []int, r []int) []int {
 }
 
 func main() {
-	randomInts := generateRandomSlice(size)
+	log.Printf("Input array size:\t\t%d", size)
 
+	randomInts := generateRandomSlice(size)
 	bubbleSort(randomInts)
 
 	randomInts = generateRandomSlice(size)
 	selectSort(randomInts)
 
 	randomInts = generateRandomSlice(size)
-
 	insertionSort(randomInts)
+
 	randomInts = generateRandomSlice(size)
-	fmt.Printf("Unsorted: %v\n", randomInts)
 	mergeSortWrapper(randomInts)
 }
